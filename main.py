dadbc321473bbb885e791a87be9dc05f325187e5
George Bilias <georgebiliasgr@gmail.com>
2023-12-05 13:41:25 +0200

    Fixed warnings

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # importing libraries
-import pygame,sys
-from settings import *
+import pygame
+import sys
+
 from level import Level
+from settings import *
 
 
 # main class
@@ -11,8 +13,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Set up the game window
         pygame.display.set_caption("Game")
         self.clock = pygame.time.Clock()  # Create a clock object to control the frame rate
-        self.level = Level() # initialise the level object
-
+        self.level = Level()  # initialise the level object
 
     def run(self):
         while True:
@@ -22,11 +23,12 @@ class Game:
                     sys.exit()  # Exit the program if the window is closed
 
             dt = self.clock.tick(60) / 1000  # Calculate the time since the last frame (in seconds)
-            self.level.run(dt) # run the level
+            self.level.run(dt)  # run the level
             # Update game logic and draw on the screen
             # (Game logic and drawing code would be added here in a complete game)
 
             pygame.display.update()  # Update the display
+
 
 # Check if the script is being run as the main program
 if __name__ == "__main__":
